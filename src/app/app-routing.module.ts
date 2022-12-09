b5eccc075cd7788332562be6d14e0ba6aa037f38
MSAAVEDRA98 <simonsnow98@gmail.com>
2022-12-09 11:53:46 -0300

    Casi ultimo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ import { PaginaErrorComponent } from './pagina-error/pagina-error.component';
 
 const routes: Routes = [
   {path: '', component: PaginaHomeComponent},
-  {path: 'home', component: PaginaHomeComponent},
-  {path: 'provincia', component: ProvinciasComponent},
+  {path: 'home', component: ProvinciasComponent},
+  // {path: 'provincia', component: ProvinciasComponent},
   {path: 'producto/:nombreProvincia', component: ProductosListComponent},
   {path: 'producto', component: ProductosListComponent},
   {path: '**', component: PaginaErrorComponent}
