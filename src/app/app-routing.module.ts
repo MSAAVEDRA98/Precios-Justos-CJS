aac61b6d3cf659ed8fa1725fddd6be14fd4063fe
MSAAVEDRA98 <simonsnow98@gmail.com>
2022-12-10 08:20:56 -0300

    Lo juro que es el ultimo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductosListComponent } from './productos-list/productos-list.component';
 import { ProvinciasComponent } from './provincias/provincias.component';
-import { PaginaHomeComponent } from './pagina-home/pagina-home.component';
 import { PaginaErrorComponent } from './pagina-error/pagina-error.component';
 
 const routes: Routes = [
-  {path: '', component: PaginaHomeComponent},
+  {path: '', component: ProvinciasComponent},
   {path: 'home', component: ProvinciasComponent},
-  // {path: 'provincia', component: ProvinciasComponent},
   {path: 'producto/:nombreProvincia', component: ProductosListComponent},
   {path: 'producto', component: ProductosListComponent},
   {path: '**', component: PaginaErrorComponent}
